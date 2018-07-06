6432ef2554a1fe9b8b83aba5470c69401ac812b5
Corey King <otamorito22@gmail.com>
2018-07-05 22:40:06 -0400

    Got the file to compile. Still working on fixing the Makefiles so that it will work on both windows and linux, but that belongs in another branch. Will work on the random issue due to placement of seed later

--- src/damascusHawk.cpp
+++ src/damascusHawk.cpp
@@ -9,16 +9,13 @@ using namespace std;
 int main() {
     vector< vector<int> > rolls(3);
 
-    rolls[0] = roll(5);
-    rolls[1] = roll(7);
-    rolls[2] = roll(15);
-
-    //rolls.push_back(roll(5));
-    //rolls.push_back(roll(7));
-    //rolls.push_back(roll(15));
+    rolls.push_back(roll(5));
+    rolls.push_back(roll(7));
+    rolls.push_back(roll(15));
 
     for(int i=0; i < rolls.size(); i++) {
-      copy(rolls.at(i).begin(), rolls.at(i).end(), ostream_iterator<char>(cout, " "));
+      copy(rolls[i].begin(), rolls[i].end(), ostream_iterator<int>(cout, " "));
+      cout << endl;
     }
     return 0;
 }
