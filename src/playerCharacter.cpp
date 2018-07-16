17522a8f466c53b56cdd7ad83a03f1e51f4685d2
Corey King <regemcaelum@live.com>
2018-07-15 22:36:56 -0400

    Made some initial progress on the creation of the class for olding the character data. More needs to be done. I have determined that I will hold the skills and attributes in their own a class.

---
+++ src/playerCharacter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class plaerCharacter {
+    public:
+        // Character Information
+        string name;
+        string sex;
+        string player;
+        string concept;
+        string chronicle;
+        string faction;
+        string groupName;
+        string description;
+        int age;
+        int virtue;
+        int vice;
+        int experience;
+        int totalExperience;
+};
