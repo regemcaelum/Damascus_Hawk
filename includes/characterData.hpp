89efd46ddfcb40087b76737e5989390b8b6cf813
Corey King <regemcaelum@users.noreply.github.com>
2019-01-31 13:04:34 -0500

    Update characterData.hpp
    changing the layout of the classes to be more modular and designed in accordance with functionality. Added class descriptor coments and some setter prototypes. Changes will have to continue later on.

--- includes/characterData.hpp
+++ includes/characterData.hpp
@@ -3,99 +3,115 @@
 
 using namespace std;
 
+/**
+ * @class characterData
+ * @brief A class containing the character information
+ * @details This is a class that contains general characteristics of a character
+ *          to include: name, sex, physical description, and age
+ */
 class characterData {
+    string name;
+    string sex;
+    string description;
+    int age;
+    
     public:
-        // Character Information
-        string name;
-        string sex;
-        string description;
-        int age;
+        characterData();
+        void setName(string name);
+        void setSex(string sex);
+        void setDescription(string description);
+        void setAge(int age);
+    
+        string getName() { return name; }
+        string getSex() { return sex; }
+        string getDescription() { return description; }
+        ing getAge() { return age; }
 };
 
-class PhysicalAttributes {
-    int priority;
-    int points;
-    int spentPoints;
-    int strength;
-    int dexterity;
-    int stamina;
-
+// TODO: move next few class to an Attributes.hpp file
+class Attributes {
+    protected:
+        int priority;
+        int points;
+        int spentPoints;
+            
     public:
-    PhysicalAttributes();
-    void setPriority(int level);
-    void determinePoints();
-    void resetAttributes();
-    int incramentAttribute();
-    int decramentAttribute();
+        void setPriority(int level);
+        void determinePoints();
+        enum class Attribute { STR, DEX, STAM, INT, WITS, RES, PRES, MANIP, COMP };
 };
 
-class MentalAttributes {
-        int priority;
-        int points;
-        int intelligence;
-        int wits;
-        int resolve;
+class PhysicalAttributes: public Attributes {
+    protected:
+        int strength;
+        int dexterity;
+        int stamina;
 };
 
-class SocialAttributes {
-        int priority;
-        int points;
-        int presence;
-        int manipulation;
-        int composure;
+class MentalAttributes: public Attributes {
+    int intelligence;
+    int wits;
+    int resolve;
 };
 
-class MentalSkills {
-    public:
-    private:
-        // Mental
-        int priority;
-        int points;
-        int academics;
-        int computer;
-        int crafts;
-        int investigation;
-        int medicine;
-        int occult;
-        int politics;
-        int science;
+class SocialAttributes: public Attributes {
+    int presence;
+    int manipulation;
+    int composure;
 };
 
-class PhysicalSkills {
-        // Physical
-        int priority;
-        int points;
-        int atheltics;
-        int brawl;
-        int drive;
-        int firearms;
-        int larceny;
-        int stealth;
-        int survival;
-        int weaponry;
-};
-
-class SocialSkills {
-        // Social
-        int priority;
-        int points;
-        int animalKen;
-        int empathy;
-        int expression;
-        int intimidation;
-        int persuasion;
-        int socialize;
-        int streetwise;
-        int subterfuge;
-};
-
-class CharacterAttributes {
+class CharacterAttributes: public MentalAttributes, public SocialAttributes, public PhysicalAttributes {
     SocialAttributes sAttributes;
     MentalAttributes mAttributes;
     PhysicalAttributes pAttributes;
+    
+    public:
+        void resetAttributes();
+        void resetAttributes(int type);
+        int incramentAttribute(Attribute attribute);
+        int decramentAttribute(int attribute);
 };
 
-class CharacterSkills {
+// TODO: move next few class to an Skills.hpp file
+class Skills {
+    protected:
+        int priority;
+        int points;
+};
+class MentalSkills: public Skills {
+    int academics;
+    int computer;
+    int crafts;
+    int investigation;
+    int medicine;
+    int occult;
+    int politics;
+    int science;
+};
+
+class PhysicalSkills: public Skills {
+    int atheltics;
+    int brawl;
+    int drive;
+    int firearms;
+    int larceny;
+    int stealth;
+    int survival;
+    int weaponry;
+};
+
+class SocialSkills: public Skills {
+    int animalKen;
+    int empathy;
+    int expression;
+    int intimidation;
+    int persuasion;
+    int socialize;
+    int streetwise;
+    int subterfuge;
+};
+
+class CharacterSkills: public MentalSkills, public SocialSkills, public PhysicalSkills {
     SocialSkills sSkills;
     MentalSkills mSkills;
     PhysicalSkills pSkills;
