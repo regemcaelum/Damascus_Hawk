a0c070d40ce88f79cd1914c565f09cbf530c878a
Corey King <regemcaelum@live.com>
2024-12-17 16:24:09 -0500

    Updated the comments on the dices tools.
    Updated with the doxygen style. If we update this code to be more in
    depth it will be easy to do so.

--- include/dice.hpp
+++ include/dice.hpp
@@ -8,6 +8,12 @@
 #define CRIT 10
 #define SUCCESS_THRESHOLD 7
 
+/*!
+    \brief Generates random numbers like dice.
+
+    \param pool     The number of random nunbers to generate.
+    \param explode  What result leads an extra die.
+*/
 std::vector<int> roll(int pool, int explode=10);
 
 #endif
--- src/dice.cpp
+++ src/dice.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+/*!
+    Uses vector and generate random number in equlivent to the value of the pool
+    parameter. If a number is greater than or equal to the explode parameter,
+    which defaults to 10, then another die is added to the pool.
+*/
 std::vector<int> roll(int pool, int explode) {
     vector<int> results(pool);
     vector<int>::iterator die;
